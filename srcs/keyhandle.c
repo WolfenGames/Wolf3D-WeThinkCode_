7464cc0a02dcc0100e2c6e4386e76d53d80324fe
Julian Wolf <julian.w16@gmail.com>
2018-07-26 14:08:03 +0200

    auto commit

--- srcs/keyhandle.c
+++ srcs/keyhandle.c
@@ -37,7 +37,7 @@ void	rotate(int key, t_wolf *w)
 	}
 }
 
-t_bool	move_off_zero(t_wolf *w)
+t_bool	move_off_zero_x(t_wolf *w)
 {
 	float	x;
 	float	y;
@@ -46,33 +46,44 @@ t_bool	move_off_zero(t_wolf *w)
 
 	x = w->p.x;
 	y = w->p.y;
-	x1 = w->p.x + w->p.dirx * 0.3f;
-	y1 = w->p.y + w->p.diry * 0.3f;
+	x1 = x + (w->p.x + w->p.dirx * 0.4f);
+	y1 = y + (w->p.y + w->p.diry * 0.4f);
 	return ((w->pnts[(int)(x1)][(int)(y)].type != 0) ||
-			(w->pnts[(int)(x)][(int)(y1)].type != 0) ||
-			(w->pnts[(int)(x1)][(int)(y)].type != 0) ||
+			(w->pnts[(int)(x)][(int)(y1)].type != 0));
+}
+
+t_bool	move_off_zero_y(t_wolf *w)
+{
+	float	x;
+	float	y;
+	float	x1;
+	float	y1;
+
+	x = w->p.x;
+	y = w->p.y;
+	x1 = x - (w->p.x + w->p.dirx * 0.4f);
+	y1 = y - (w->p.y + w->p.diry * 0.4f);
+	return ((w->pnts[(int)(x1)][(int)(y)].type != 0) ||
 			(w->pnts[(int)(x)][(int)(y1)].type != 0));
 }
 
 void	move_x_y(int key, t_wolf *w)
 {
-	if (!move_off_zero(w))
+	if ((key == W || key == ARROW_UP))
 	{
-		if (key == W || key == ARROW_UP)
-		{
-			if (w->pnts[(int)(w->p.x + w->p.dirx * 0.1f)][(int)(w->p.y)].type < 1)
-				w->p.x += w->p.dirx * 0.3f;
-			if (w->pnts[(int)(w->p.x)][(int)(w->p.y + w->p.diry * 0.1f)].type < 1)
-				w->p.y += w->p.diry * 0.3f;
-		}
-		if (key == S || key == ARROW_DOWN)
-		{
-			if (w->pnts[(int)(w->p.x + w->p.dirx * 0.1f)][(int)(w->p.y)].type < 1)
-				w->p.x -= w->p.dirx * 0.3f;
-			if (w->pnts[(int)(w->p.x)][(int)(w->p.y + w->p.diry * 0.1f)].type < 1)
-				w->p.y -= w->p.diry * 0.3f;
-		}
+		if (w->pnts[(int)(w->p.x + w->p.dirx * 0.1f)][(int)(w->p.y)].type < 1)
+			w->p.x += w->p.dirx * 0.3f;
+		if (w->pnts[(int)(w->p.x)][(int)(w->p.y + w->p.diry * 0.1f)].type < 1)
+			w->p.y += w->p.diry * 0.3f;
 	}
+	if ((key == S || key == ARROW_DOWN))
+	{
+		if (w->pnts[(int)(w->p.x + w->p.dirx * 0.1f)][(int)(w->p.y)].type < 1)
+			w->p.x -= w->p.dirx * 0.3f;
+		if (w->pnts[(int)(w->p.x)][(int)(w->p.y + w->p.diry * 0.1f)].type < 1)
+			w->p.y -= w->p.diry * 0.3f;
+	}
+
 }
 
 int		key_press_hook(int key, t_wolf *w)
@@ -85,7 +96,6 @@ int		key_press_hook(int key, t_wolf *w)
 		w->fog -= 1;
 	move_x_y(key, w);
 	rotate(key, w);
-	move_off_zero(w);
 	return (0);
 }
 
